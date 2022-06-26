ad48b2b3514c455c7e13ae24525ead1444b34156
Simone Festa <55951548+simonefesta@users.noreply.github.com>
2022-06-26 17:05:49 +0200

    increase tests & coverage on specificData

--- lang/java/avro/src/test/java/org/apache/avro/specific/TestGetSchemaNameSpecificData.java
+++ lang/java/avro/src/test/java/org/apache/avro/specific/TestGetSchemaNameSpecificData.java
@@ -1,11 +1,19 @@
 package org.apache.avro.specific;
 
+import com.sun.org.apache.xml.internal.utils.URI;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.File;
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -26,12 +34,37 @@ public class TestGetSchemaNameSpecificData {
   }
 
   @Parameterized.Parameters
-  public static Collection<?> getParameter() {
+  public static Collection<?> getParameter() throws MalformedURLException, URI.MalformedURIException {
+    // creating complex types, see :
+    // https://avro.apache.org/docs/current/spec.html#schema_primitive
 
-    return Arrays.asList(new Object[][] { { "int", 5 }, { "null", null }, { "map", new HashMap<>() },
-        { "boolean", true }, { null, new Object() }, { "float", 3.2f },
+    String strRecord = "{\"namespace\": \"record\",\n" + " \"type\": \"record\",\n" + " \"name\": \"example\",\n"
+        + " \"fields\": [\n" + "     {\"name\": \"name\", \"type\": \"string\"}\n" + " ]\n" + "}";
+    Schema schemaRecord = new Schema.Parser().parse(strRecord);
+
+    String strEnum = "{\"type\": \"enum\",\n" + "  \"name\": \"enum\",\n"
+        + "  \"symbols\" : [\"ARRAY\", \"INT\", \"SPADES\", \"HEARTS\"]\n" + "}";
+    Schema schemaEnum = new Schema.Parser().parse(strEnum);
+
+    String strArray = "{\"type\": \"array\",\n" + "\"items\" : \"string\"}";
+    Schema schemaArray = new Schema.Parser().parse(strArray);
+
+    String strFixed = "{\"type\": \"fixed\",\n" + " \"size\": 16,\n" + " \"name\": \"fixed\"}";
+    Schema schemaFixed = new Schema.Parser().parse(strFixed);
+    byte[] supportFixedByte = new byte[16];
+    String bigIntegerVal = "2";
+    String url = "http://www.example.com/docs/resource1.html";
+
+    return Arrays.asList(new Object[][] { { "null", null }, { "boolean", true }, { "int", 5 }, { "long", 2147483649L },
+        { "float", 3.2f }, { "double", 581216732.323433 }, { "bytes", ByteBuffer.allocate(1) }, { "string", "s" },
+        // complex type
+        { "record.example", new GenericData.Record(schemaRecord) },
+        { "enum", new GenericData.EnumSymbol(schemaEnum, strEnum) },
+        { "array", new GenericData.Array<>(2, schemaArray) }, { "map", new HashMap<>() },
+        { "fixed", new GenericData.Fixed(schemaFixed, supportFixedByte) }, { null, new Object() },
         // coverage
-        { "string", new BigDecimal(2) }
+        { "string", new BigDecimal(2) }, { "string", new File("src/test") },
+        { "string", new BigInteger(bigIntegerVal) }, { "string", new URL(url) },
 
     });
 
--- lang/java/avro/src/test/java/org/apache/avro/specific/TestGetSchemaSpecificData.java
+++ lang/java/avro/src/test/java/org/apache/avro/specific/TestGetSchemaSpecificData.java
@@ -1,10 +1,10 @@
 package org.apache.avro.specific;
 
-import org.apache.avro.AvroRuntimeException;
 import org.apache.avro.AvroTypeException;
 import org.apache.avro.Schema;
 
 import org.junit.Assert;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.ArrayList;
@@ -40,18 +40,22 @@ public class TestGetSchemaSpecificData {
 
     return Arrays.asList(new Object[][] { { Schema.Type.INT, Integer.class }, { Schema.Type.BOOLEAN, Boolean.class },
         { Schema.Type.FLOAT, Float.class }, { Schema.Type.DOUBLE, Double.class }, { Schema.Type.STRING, String.class },
-        { AvroTypeException.class, null }, { Schema.Type.BYTES, ByteBuffer.class }, // tipi di
+        { AvroTypeException.class, null }, { Schema.Type.LONG, Long.class }, { Schema.Type.NULL, Void.class },
+        { Schema.Type.BYTES, ByteBuffer.class },
+
+        // tipi di
         // dati
         // complessi
         { Schema.Type.ARRAY, TestGetSchemaSpecificData.class.getDeclaredField("array").getGenericType() },
-        { Schema.Type.MAP, TestGetSchemaSpecificData.class.getDeclaredField("map").getGenericType() },
-        { AvroRuntimeException.class, java.io.IOException.class }, { Schema.Type.NULL, Void.class }, });
+        { Schema.Type.MAP, TestGetSchemaSpecificData.class.getDeclaredField("map").getGenericType() }, });
+
   }
 
   @Test
   public void testGetSchema() {
     Schema.Type actual;
     try {
+
       actual = SpecificData.get().getSchema(type).getType();
 
       Assert.assertEquals(expected, actual);
